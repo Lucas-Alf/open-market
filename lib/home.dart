a58216fe41ac3372a8c0c0389539d2256ac0a650
GabrielFim <gabifimtm@gmail.com>
2020-11-22 15:48:29 -0300

    Categorias?

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,20 @@ class _HomeState extends State<Home> {
   TextEditingController _searchQueryController = TextEditingController();
   bool _isSearching = false;
   String searchQuery = "Search query";
+  int paginaCorrente=0;
+
+  final List<Widget> paginas = [
+    //Home(),
+    //Favoritos(),
+    //Compras(),
+    //Conta(),
+  ];
+
+  void quandoPressionado(int index){
+    setState(() {
+      paginaCorrente = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +62,115 @@ class _HomeState extends State<Home> {
                 );
               }).toList(),
             ),
-
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      color: Colors.white,
+                      textColor: Colors.black,
+                      child: Icon(
+                        Icons.local_mall,
+                        size: 25,
+                      ),
+                      padding: EdgeInsets.all(15),
+                      shape: CircleBorder(side: BorderSide(
+                        color: Colors.grey, width: 1.0, style: BorderStyle.solid,
+                      )),
+                    ),
+                    Text('Moda')
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      color: Colors.white,
+                      textColor: Colors.black,
+                      child: Icon(
+                        Icons.stay_current_portrait,
+                        size: 25,
+                      ),
+                      padding: EdgeInsets.all(15),
+                      shape: CircleBorder(side: BorderSide(
+                        color: Colors.grey, width: 1.0, style: BorderStyle.solid,
+                      )),
+                    ),
+                    Text('Celulares')
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      color: Colors.white,
+                      textColor: Colors.black,
+                      child: Icon(
+                        Icons.sports_motorsports,
+                        size: 25,
+                      ),
+                      padding: EdgeInsets.all(15),
+                      shape: CircleBorder(side: BorderSide(
+                        color: Colors.grey, width: 1.0, style: BorderStyle.solid,
+                      )),
+                    ),
+                    Text('Motos')
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {},
+                      color: Colors.white,
+                      textColor: Colors.black,
+                      child: Icon(
+                        Icons.add,
+                        size: 25,
+                      ),
+                      padding: EdgeInsets.all(15),
+                      shape: CircleBorder(side: BorderSide(
+                        color: Colors.grey, width: 1.0, style: BorderStyle.solid,
+                      )),
+                    ),
+                    Text('Mais')
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.black,
+        onTap: quandoPressionado,
+        currentIndex: paginaCorrente,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favoritos",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Compras",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Conta",
+          ),
+
+        ],
       ),
     );
   }
