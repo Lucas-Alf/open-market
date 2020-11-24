62298b42f3480d821f4d79fe18275e99a09479bb
GabrielFim <gabifimtm@gmail.com>
2020-11-24 11:28:16 -0300

    Alguns pontos da tela de detalhe

--- lib/produto.dart
+++ lib/produto.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -37,6 +39,70 @@ class _ProdutoState extends State<Produto> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Detalhes do Produto"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Imagem:
+            Container(
+
+            ),
+            SizedBox(height: 20,),
+            //Preço/Compartilhar
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Row(children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("R\$ "+produtoValor.toString(),style:TextStyle(fontWeight: FontWeight.bold, fontSize: 20) ,textAlign: TextAlign.start),
+                          Text("à vista no boleto",style:TextStyle(color: Colors.green, fontSize: 18) ,textAlign: TextAlign.start),
+                        ],
+                      )
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                     Container(
+                        width: 35,
+                        alignment: Alignment.centerRight,
+                        child: RawMaterialButton(
+                          onPressed: () {},
+                          child: Icon(
+                            Icons.share_outlined,
+                            size: 30,
+                          ),
+                          shape: CircleBorder(
+                              side: BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              )),
+                        ),
+                      )
+                  ]),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            //Descrição
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Descrição",style:TextStyle(fontWeight: FontWeight.bold, fontSize: 22) ,textAlign: TextAlign.start),
+                  Text(produtoDescricao, style: TextStyle(fontSize: 19), textAlign: TextAlign.start,),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            //Quantidade
+          ],
+        ),
       ),
     );
   }
